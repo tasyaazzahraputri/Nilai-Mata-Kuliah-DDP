94f571ced0ff537e52d94c1a5985b6b991b3af0e
TASYA AZZAHRA PUTRI <92432388+tasyaazzahraputri@users.noreply.github.com>
2021-11-15 09:50:13 +0700

    Add files via upload

---
+++ Nilai Mata Kuliah DDP.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+	float nilai_DDP;
+	
+	cout<<"##############Nilai Mata Kuliah DDP#############\n";
+	cout<<"Masukkan Nilai Mata Kuliah DDP anda: ";cin>>nilai_DDP;
+
+
+	if (nilai_DDP < 50){
+		cout <<"F\n";
+	}else if (nilai_DDP <= 56){
+		cout <<"E\n";
+	}else if (nilai_DDP <= 61){
+		cout << "D\n";
+	}else if (nilai_DDP <= 66){
+		cout << "C\n";
+	}else if (nilai_DDP <= 71){
+		cout << "B\n";
+	}else if (nilai_DDP < 76){
+		cout << "B+\n";
+	}else {
+		cout << "A\n";
+	}
+	
+	return 0;
+}
